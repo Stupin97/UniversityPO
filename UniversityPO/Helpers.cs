51c0c233df812ce90dd032d0328a5c78004cdcb4
Ilya <stupin.il@yandex.ru>
2021-06-11 00:30:57 +0700

    Добавление метода получения времени авторизации

--- UniversityPO/Helpers.cs
+++ UniversityPO/Helpers.cs
@@ -2,11 +2,22 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
     using System.Threading.Tasks;
 
     public static class Helpers
     {
-        
+        public static string GetDateTime(DateTime? date)
+        {
+            string pattern = "MM-dd-yy";
+
+            if (date == null)
+            {
+                throw new ArgumentNullException(nameof(date));
+            }
+
+            return "Вход выполнен в: " + date.Value.ToString(pattern, CultureInfo.CreateSpecificCulture("ru-RU"));
+        }
     }
 }
